dbb0770812ccde28289af5082e4060301c6c28db
Sani Mridha <sanimridha307@gmail.com>
2020-11-14 12:30:09 +0600

    Building the listing screen

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import Screen from './app/components/Screen'
 import Icon from './app/components/Icon'
 import ListItem from './app/components/ListItem';
 import AccountScreen from './app/screens/AccountScreen';
+import ListingsScreen from './app/screens/ListingsScreen';
 
 
 export default function App() {
@@ -74,9 +75,10 @@ export default function App() {
       //<MessagesScreen/>
      //<ListingDetailsScreen/>
      //<ViewImageScreen/>
-    <Screen>
-       <AccountScreen/>
-    </Screen>
+    // <Screen>
+    //    <AccountScreen/>
+    // </Screen>
+    <ListingsScreen/>
 
     );
 }
---
+++ app/screens/ListingsScreen.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
+import ListItem from '../components/ListItem'
+import Card from '../components/Card'
+import Screen from '../components/Screen'
+import colors from '../config/colors'
+
+const listing=[
+    {
+        id:1,
+        title:"Red Jacket for sale",
+        price:100,
+        image:require('../assets/jacket.jpg')
+    },
+    {
+        id:2,
+        title:"Couch in great Condition",
+        price:1000,
+        image:require('../assets/couch.jpg')
+    },
+]
+const ListingsScreen = () => {
+    return (
+        <Screen style={styles.screen}>
+            <FlatList
+                data={listing}
+                keyExtractor={listing => listing.id.toString()}
+                renderItem={({item}) =>
+                    <Card
+                        title={item.title}
+                        subTitle={"$"+ item.price}
+                        image={item.image}
+                    />
+                }
+            />
+        </Screen>
+    )
+}
+
+const styles = StyleSheet.create({
+    screen:{
+        padding:20,
+        backgroundColor:colors.light,
+    }
+})
+
+export default ListingsScreen
