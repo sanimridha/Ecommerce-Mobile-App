496903ad22b8e97e201c43c79f6fad7d163d0a6c
Sani Mridha <sanimridha307@gmail.com>
2020-11-15 10:14:13 +0600

    Build a new config called styles.js that contains all default styles with color.js

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Text, StyleSheet, Platform } from 'react-native'
+import { Text } from 'react-native'
+import defaultStyles from '../config/styles'
 function AppText({children, style}) {
     return (
-    <Text style={[styles.text, style]}>{children}</Text>
+    <Text style={[defaultStyles.text, style]}>{children}</Text>
     );
 }
-const styles = StyleSheet.create({
-    text:{
-        fontSize:20,
-    }
-})
+
 
 export default AppText;
--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -3,29 +3,25 @@ import { Platform, StyleSheet, Text, TextInput, View } from 'react-native'
 
 import {MaterialCommunityIcons} from "@expo/vector-icons"
 import colors from '../config/colors'
+import defaultStyles from '../config/styles'
 
 const AppTextInput = ({icon, ...otherPros}) => {
     return (
         <View style={styles.container}>
            { icon && <MaterialCommunityIcons name={icon}size={25} style={styles.icons}/>}
-           <TextInput style={styles.textInput} {...otherPros}/>
+           <TextInput style={defaultStyles.text} {...otherPros}/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        backgroundColor:colors.light,
+        backgroundColor:defaultStyles.colors.light,
         borderRadius:25,
         flexDirection:"row",
         width:"100%",
         padding:15,
         marginVertical:10,
-    },
-    textInput:{
-        color: colors.dark,
-        fontSize:18,
-        fontFamily: Platform.OS === 'android'? "Roboto":"Anenir"
     },
     icons:{
         marginRight:10,
