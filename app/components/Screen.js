9d345f4577bc0022e5daaf3b3f451868276bb4d3
Sani Mridha <sanimridha307@gmail.com>
2020-11-18 13:27:51 +0600

    Improving the Message Screen

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -3,12 +3,19 @@ import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import Constants from "expo-constants";
 
 const Screen = ({ children, style }) => {
-  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
+  return (
+    <SafeAreaView style={[styles.screen, style]}>
+      <View style={[style, styles.view]}>{children}</View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
   screen: {
     paddingTop: Constants.statusBarHeight,
+    flex: 1,
+  },
+  view: {
     flex: 1,
   },
 });
