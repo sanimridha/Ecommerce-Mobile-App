b8f30586b3a565a668a819274a7ade74ecdde250
Sani Mridha <sanimridha307@gmail.com>
2020-11-29 12:48:39 +0600

    adding account navigator

--- app/navigation/FeedNavigator.jsx
+++ app/navigation/FeedNavigator.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import ListingsScreen from "../screens/ListingsScreen";
 import ListingDetailsScreen from "../screens/ListingDetailsScreen";
+import { Vibration } from "react-native";
 
 const Stack = createStackNavigator();
 
 const FeedNavigator = () => (
-  <Stack.Navigator>
+  <Stack.Navigator mode="modal">
     <Stack.Screen name="Listings" component={ListingsScreen} />
     <Stack.Screen name="ListingDetails" component={ListingDetailsScreen} />
   </Stack.Navigator>
