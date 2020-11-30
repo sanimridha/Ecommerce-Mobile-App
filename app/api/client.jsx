97f31b27b30a186b273bb22077939539e5472615
Sani Mridha <sanimridha307@gmail.com>
2020-11-30 12:48:18 +0600

    export apiClient

--- app/api/client.jsx
+++ app/api/client.jsx
@@ -4,8 +4,4 @@ const apiClient = create({
   baseURL: "http://192.168.0.118/api",
 });
 
-apiClient.get("/listings").then(response => {
-  if (!response.ok) {
-    response.problem;
-  }
-});
+export default apiClient;
