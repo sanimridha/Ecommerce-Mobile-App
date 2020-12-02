f5950c456036e0a8b0ccbb148c6d152e96603996
Sani Mridha <sanimridha307@gmail.com>
2020-12-02 12:48:45 +0600

    handling error in listing page

--- app/screens/ListingDetailsScreen.jsx
+++ app/screens/ListingDetailsScreen.jsx
@@ -5,8 +5,11 @@ import AppText from "../components/AppText";
 import colors from "../config/colors";
 import ListItem from "../components/ListItem";
 
+import ListingsScreen from "./ListingsScreen";
+
 const ListingDetailsScreen = ({ route }) => {
   const listing = route.params;
+
   return (
     <View>
       <Image style={styles.image} source={listing.image} />
@@ -16,7 +19,6 @@ const ListingDetailsScreen = ({ route }) => {
         <View style={styles.userContainer}>
           <ListItem
             image={require("../assets/sani3.png")}
-            // image='../assets/Sani.jpg'
             title="Sani Mridha"
             subTitle="5 Listings"
           />
--- app/screens/ListingsScreen.jsx
+++ app/screens/ListingsScreen.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, FlatList, Text, View } from "react-native";
 import Card from "../components/Card";
+
 import Screen from "../components/Screen";
 import colors from "../config/colors";
 import routes from "../navigation/routes";
 import listingApi from "../api/listings";
+import AppText from "../components/AppText";
+import AppButton from "../components/AppButton";
 
 const ListingsScreen = ({ navigation }) => {
   const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     loadListings();
@@ -15,10 +19,20 @@ const ListingsScreen = ({ navigation }) => {
 
   const loadListings = async () => {
     const response = await listingApi.getListings();
+    if (!response.ok) return setError(true);
+    setError(false);
     setListings(response.data);
   };
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <View style={styles.error}>
+            <AppText>Couldn't retrive the listings.</AppText>
+            <AppButton title="Retry" onPress={loadListings} />
+          </View>
+        </>
+      )}
       <FlatList
         data={listings}
         keyExtractor={listing => listing.id.toString()}
@@ -39,6 +53,11 @@ const styles = StyleSheet.create({
   screen: {
     padding: 20,
     backgroundColor: colors.light,
+  },
+  error: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 300,
   },
 });
 
