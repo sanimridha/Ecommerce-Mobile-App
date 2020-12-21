bc2c24056c85ae1c35957708d6422f8a36751335
Sani Mridha <sanimridha307@gmail.com>
2020-12-21 11:55:34 +0600

    adding NetInfo to check real time network status

--- app/components/OfflineNotice.jsx
+++ app/components/OfflineNotice.jsx
@@ -3,13 +3,18 @@ import { StyleSheet, View } from "react-native";
 import colors from "../config/colors";
 import AppText from "./AppText";
 import Constants from "expo-constants";
+import { useNetInfo } from "@react-native-community/netinfo";
 
 const OfflineNotice = props => {
-  return (
-    <View style={styles.container}>
-      <AppText style={styles.text}>No Internet Connection</AppText>
-    </View>
-  );
+  const netInfo = useNetInfo();
+
+  if (netInfo.type !== "unknown" && netInfo.isInternetReachable === false)
+    return (
+      <View style={styles.container}>
+        <AppText style={styles.text}>No Internet Connection</AppText>
+      </View>
+    );
+  return null;
 };
 
 const styles = StyleSheet.create({
