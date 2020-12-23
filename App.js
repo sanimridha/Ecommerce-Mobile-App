226aed695ef0b7c84fc5a3a37eeb08e0f4860e16
Sani Mridha <sanimridha307@gmail.com>
2020-12-23 11:59:49 +0600

    Refactor some codes

--- App.js
+++ App.js
@@ -1,134 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  Switch,
-  Button,
-  Image,
-} from "react-native";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import AppText from "./app/components/AppText";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import AppButton from "./app/components/AppButton";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
-import LoginScreen from "./app/screens/LoginScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-
-import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
-import FormImagePicker from "./app/components/forms/FormImagePicker";
-
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import RegisterScreen from "./app/screens/RegisterScreeen";
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
-import NetInfo from "@react-native-community/netinfo";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import OfflineNotice from "./app/components/OfflineNotice";
 import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
-import jwtDecode from "jwt-decode";
 import { AppLoading } from "expo";
-// const Link = () => {
-//   const navigation = useNavigation();
-//   return (
-//     <Button
-//       title="Click"
-//       onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
-//     />
-//   );
-// };
 
-// const Tweets = ({ navigation }) => (
-//   <Screen>
-//     <Text>Tweet</Text>
-//     <Link />
-//   </Screen>
-// );
-// const TweetDetails = ({ route }) => (
-//   <Screen>
-//     <Text>Tweet Details{route.params.id}</Text>
-//   </Screen>
-// );
-// const Account = () => (
-//   <Screen>
-//     <Text>Account Screen</Text>
-//   </Screen>
-// );
-
-// const Stack = createStackNavigator();
-// const FeedNavigator = () => (
-//   <Stack.Navigator
-//     screenOptions={{
-//       headerStyle: { backgroundColor: "dodgerblue" },
-//       headerTintColor: "white",
-//     }}
-//   >
-//     <Stack.Screen name="Tweets" component={Tweets} />
-//     <Stack.Screen
-//       name="TweetDetails"
-//       component={TweetDetails}
-//       options={({ route }) => ({ title: route.params.id })}
-//     />
-//   </Stack.Navigator>
-// );
-// const Tab = createBottomTabNavigator();
-// const TabNavigator = () => (
-//   <Tab.Navigator
-//     tabBarOptions={{
-//       activeBackgroundColor: "tomato",
-//       activeTintColor: "#fff",
-//       inactiveBackgroundColor: "#eee",
-//       inactiveTintColor: "black",
-//     }}
-//   >
-//     <Tab.Screen
-//       name="Feed"
-//       component={FeedNavigator}
-//       options={{
-//         tabBarIcon: ({ size, color }) => (
-//           <MaterialCommunityIcons name="home" size={size} color={color} />
-//         ),
-//       }}
-//     />
-//     <Tab.Screen name="Account" component={Account} />
-//   </Tab.Navigator>
-// );
 export default function App() {
   const [user, setUser] = useState();
   const [isReady, setIsReady] = useState(false);
 
-  const restoreToken = async () => {
-    const token = await authStorage.getToken();
-    if (!token) return;
-    setUser(jwtDecode(token));
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
   };
   if (!isReady)
     return (
-      <AppLoading startAsync={restoreToken} onFinish={() => setIsReady(true)} />
+      <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} />
     );
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       <NavigationContainer theme={navigationTheme}>
-        {/* <AppNavigator /> */}
         {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
       <OfflineNotice />
