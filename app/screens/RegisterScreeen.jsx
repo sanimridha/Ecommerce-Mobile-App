f0954521520a0dd27f3b8232374f8cd848bf8992
sanimridha <sanimridha307@gmail.com>
2021-01-05 13:45:20 +0600

    implemenating the registration feature

--- app/screens/RegisterScreeen.jsx
+++ app/screens/RegisterScreeen.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
+import usersApi from "../api/users";
 
 import Screen from "../components/Screen";
 import { AppForm, AppFormField, SubmitButton } from "../components/forms";
+import useAuth from "../auth/useAuth";
+import authApi from "../api/auth";
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required().label("Name"),
@@ -12,6 +15,28 @@ const validationSchema = Yup.object().shape({
 });
 
 function RegisterScreen() {
+  const auth = useAuth();
+  const [error, setError] = useState();
+
+  const handleSubmit = async userInfo => {
+    const result = await usersApi.register(userInfo);
+
+    if (!result.ok) {
+      if (result.data) setError(result.data.error);
+      else {
+        setError("Ann Unexpected error occurred.");
+        console.log(result);
+      }
+
+      return;
+    }
+    const { data: authToken } = await authApi.login(
+      userInfo.email,
+      userInfo.password
+    );
+    auth.logIn(authToken);
+  };
+
   return (
     <Screen style={styles.container}>
       <AppForm
